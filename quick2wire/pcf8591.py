060d0a58c4bca5d7ce05aaac65aafec194b607b8
Romilly Cocking <romilly.cocking@gmail.com>
2019-12-08 14:38:51 +0000

    adding pcf8591

--- quick2wire/pcf8591.py
+++ quick2wire/pcf8591.py
@@ -5,21 +5,23 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 address = 0x48
 
-def read(chn): #channel
-	try:
-		if chn == 0:
-			bus.write_byte(address,0x40)
-		if chn == 1:
-			bus.write_byte(address,0x41)
-		if chn == 2:
-			bus.write_byte(address,0x42)
-		if chn == 3:
-			bus.write_byte(address,0x43)
-		bus.read_byte(address) # dummy read to start conversion
-	except Exception as e:
-		print ("Address: %s" % address)
-		print (e)
-	return bus.read_byte(address)
+bus = busio.I2C(board.SCL, board.SDA)
+
+_BUFFER = bytearray(3)
+
+class PCF8591():
+    def _read_u8(self, register):
+        # Read an unsigned 8 bit value from the specified 8-bit register.
+        with self._device as i2c:
+            _BUFFER[0] = register & 0xFF
+
+            i2c.write_then_readinto(_BUFFER, _BUFFER, out_end=1, in_start=1, in_end=2)
+            return _BUFFER[1]
+
+    def read(self):
+        return self._read_u8(0x40)
+
+chip = PCF8591()
 
 while True:
-    print(read(0))
+    print(chip.read())
